c526404266ce59669e396f0e05501f2bc75d6032
Nicole Manibog <nmxnii>
2024-03-04 17:56:49 +0800

    addded models, updated htmls, added admin panel

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Ingredient, Recipe, RecipeIngredient
+class RecipeInLine(admin.TabularInline):
+    model = Recipe
+class IngredientInLine(admin.TabularInline):
+    model = Ingredient
+class RecipeInInLine(admin.TabularInline):
+    model = RecipeIngredient
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [RecipeInInLine, ] 
+    list_display = ['name'] 
 
+class IngredientAdmin(admin.ModelAdmin): 
+    model = Ingredient
+
+    list_display = ['name']
 # Register your models here.
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db import models
+from django.urls import reverse
+class Ingredient(models.Model): 
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('Ingredient_detail', args=[str(self.name)]) #what do i put in what task_detail is
+class Recipe(models.Model): 
+    name = models.CharField(max_length=100) 
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('ledger:recipe-detail', args=[str(self.name)]) #what do i put in what task_detail is
+class RecipeIngredient(models.Model): 
+    quantity = models.CharField(max_length=100)
+    ingredientF = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe')
+    recipeF = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients' )
 
-# Create your models here.
+
+
