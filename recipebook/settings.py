8c644e307a98e0994ed71d8a10bb961e4da7c583
Nicole Manibog <nmxnii>
2024-03-15 13:46:22 +0800

    settings and url modified

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ledger',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/recipes/list'
+LOGOUT_REDIRECT_URL='/accounts/login'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
